0b8ac34497e83d9ae221a55e7907c5a3af964d7c
SreeSindhuSruthi <sssindh@gmail.com>
2015-09-06 18:50:37 +0530

    Base project structure

---
+++ app/views.py
@@ -0,0 +1,25 @@
+from flask import render_template
+from app import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+    #return "Hello, World!"
+    
+    user = {'name': 'Sruthi'}  # fake user
+    posts = [  # fake array of posts
+        { 
+            'author': {'name': 'Sruthi'}, 
+            'body': 'Welcome to orgab!' 
+        },
+        { 
+            'author': {'name': 'Sujay'}, 
+            'body': "That's cool!"
+        }
+    ]
+    return render_template("index.html",
+                           title='Home',
+                           user=user,
+                           posts=posts)
+
+    #http://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-x-full-text-search
---
+++ flask/Scripts/pydoc.py
@@ -0,0 +1,4 @@
+#!D:\electronics\Webapp\orgad\flask\Scripts\python.exe
+if __name__ == '__main__':
+    import sys, pydoc
+    sys.exit(pydoc.cli())
